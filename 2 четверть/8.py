b5cf268a34e2ae51704f257364389a69c3833c27
Владос <114632612+VladOsIkkkk@users.noreply.github.com>
2023-02-25 10:33:20 +0700

    Create 8.py

---
+++ 2 четверть/8.py
@@ -0,0 +1,15 @@
+from itertools import product
+nums=product ('01234567', repeat=5)
+k=0
+n='16 36 56 76 61 63 65 67'
+nn=n.split()
+for n in nums:
+    numb=''.join(n)
+    sp=[]
+    if numb.count('6')==1 and numb[0]!='0':
+        for x in nn:
+            if x in numb:
+                sp.append(x)
+        if not sp:
+            k+=1
+print(k)
