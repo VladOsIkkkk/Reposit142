3c8b6d0d620c185e105707463d63c8232e5f8486
Владос <114632612+VladOsIkkkk@users.noreply.github.com>
2023-02-25 10:27:43 +0700

    Create 23min.py

---
+++ 2 четверть/23min.py
@@ -0,0 +1,13 @@
+from itertools import product
+for i in range (1,6):
+    b=product('12',repeat=i)
+    for n in b:
+        prog="".join(n)
+        a=12
+        for x in prog:
+            if x=='1':
+                a-=1
+            elif x=='2':
+                a=a*7
+        if a ==489:
+            print(prog)
